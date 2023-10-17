e91d9cd9c19a92795e4cc67874ca044bdbed2467
sineka-mediwave <sineka@mindwaveventures.com>
2023-10-17 17:22:37 +0530

    three sections

--- app/README.md
+++ app/README.md
@@ -1,1 +1,3 @@
 # TinyDrello
+
+- First, creating 3 sections for a UI design using bootstrap.
--- app/src/App.jsx
+++ app/src/App.jsx
@@ -1,8 +1,15 @@
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./App.css";
 function App() {
   return (
-    <>
+    <div className="container">
       <h1>Drello</h1>
-    </>
+      <div className="row ">
+        <div className="col-sm taskDiv">Todo</div>
+        <div className="col-sm taskDiv">In Progress</div>
+        <div className="col-sm taskDiv">Completed</div>
+      </div>
+    </div>
   );
 }
 
